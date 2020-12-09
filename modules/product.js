e9ffc85a2e9b69275d3a57466b954f9d41ebe295
LYJ <dudwo0129@naver.com>
2020-12-10 01:43:57 +0900

    스키마 정의

---
+++ modules/product.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const productSchma = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: Array,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = mongoose.model('Product', productSchma);
+
+module.exports = { Product };
