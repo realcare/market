f6091af5612468d00da0419b3e1a30a2b9a3f765
LYJ <dudwo0129@naver.com>
2020-12-11 17:34:35 +0900

    profile router

--- routes/profile.js
+++ routes/profile.js
@@ -1,33 +1,25 @@
 const express = require('express');
 const { User } = require('../modules/user');
 const isAuth = require('../middleware/isAuth');
-
+const middleMulter = require('../middleware/middleMulter');
+const { Product } = require('../modules/product');
 const router = express.Router();
 
 router.get('/', isAuth, async (req, res, next) => {
   try {
-    await res.render('profile', {
-      user: {
-        email: req.user.email,
-        nickName: req.user.nickName,
-        userName: req.user.userName,
-        phoneNum: req.user.phoneNum,
-        dateOfBirth: req.user.dateOfBirth,
-      },
-    });
+    if (req.user) {
+      res.render('profile', {
+        user: req.user,
+      });
+    } else {
+      return res.send(
+        '<script type="text/javascript">alert("로그인이 필요합니다."); window.location="/"; </script>'
+      );
+    }
   } catch (error) {
     console.error(error);
     next(error);
   }
-});
-
-router.get('/delete', isAuth, async (req, res, next) => {
-  await User.findByIdAndDelete({ _id: req.user._id }, (err, user) => {
-    if (err) return next(err);
-    return res.send(
-      '<script type="text/javascript">alert("회원탈퇴가 완료되었습니다 ."); window.location="/"; </script>'
-    );
-  });
 });
 
 router
@@ -40,7 +32,7 @@ router
         });
       } else {
         return res.send(
-          '<script type="text/javascript">alert("로그인된 회원이 아닙니다 ."); window.location="/"; </script>'
+          '<script type="text/javascript">alert("로그인된 회원이 아닙니다."); window.location="/"; </script>'
         );
       }
     } catch (error) {
@@ -48,17 +40,46 @@ router
       next(error);
     }
   })
-  .post(isAuth, async (req, res, next) => {
-    await User.findByIdAndUpdate(
-      { _id: req.user._id },
-      { User: req.body },
-      (err) => {
-        if (err) return next(err);
-        return res.send(
-          '<script type="text/javascript">alert("회원정보 수정완료 했습니다 ."); window.location="/"; </script>'
+  .post(
+    isAuth,
+    middleMulter('profileImg').single('profileImg'),
+    async (req, res, next) => {
+      try {
+        let img;
+        if (req.file) {
+          img = req.file.path.slice('6');
+        } else {
+          img = '/image/noimg.jpg';
+        }
+        const user = await User.findOneAndUpdate(
+          { _id: req.user._id },
+          {
+            userName: req.body.userName,
+            nickName: req.body.nickName,
+            phoneNum: req.body.phoneNum,
+            profileImg: img,
+          },
+          { new: true }
         );
+        return res.render('profile', { user });
+      } catch (error) {
+        console.error(error);
+        next(error);
       }
+    }
+  );
+
+router.get('/delete', isAuth, async (req, res, next) => {
+  try {
+    await Product.deleteMany({ author: req.user.id });
+    await User.findByIdAndDelete({ _id: req.user.id });
+    return res.send(
+      '<script type="text/javascript">alert("회원탈퇴가 완료되었습니다 ."); window.location="/"; </script>'
     );
-  });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 module.exports = router;
