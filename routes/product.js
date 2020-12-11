7a3953152fbf33c9d9c2e22bc88483463b5d2e62
LYJ <dudwo0129@naver.com>
2020-12-11 17:19:56 +0900

    multer 미들 웨어 사용 ,  /:id을 사용하여 update, delete 구현

--- routes/product.js
+++ routes/product.js
@@ -1,30 +1,8 @@
 const express = require('express');
-const isAuth = require('../middleware/isAuth');
 const router = express.Router();
-const multer = require('multer');
-const fs = require('fs');
+const isAuth = require('../middleware/isAuth');
+const middleMulter = require('../middleware/middleMulter');
 const { Product } = require('../modules/product');
-const path = require('path');
-
-try {
-  fs.readdirSync('public/uploads');
-} catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다');
-  fs.mkdirSync('public/uploads');
-}
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, 'public/uploads/');
-    },
-    filename(req, file, done) {
-      const ext = path.extname(file.originalname);
-      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-});
 
 router.get('/', isAuth, async (req, res, next) => {
   try {
@@ -35,16 +13,19 @@ router.get('/', isAuth, async (req, res, next) => {
         v.img[0];
         console.log(v.img[0]);
       });
-      res.render('product', {
-        user: {
-          email: req.user.email,
-          nickName: req.user.nickName,
-          userName: req.user.userName,
-          phoneNum: req.user.phoneNum,
-          dateOfBirth: req.user.dateOfBirth,
-        },
-        products,
-      });
+      if (!req.user) {
+        return res.render('main');
+      } else {
+        res.render('product', {
+          user: {
+            email: req.user.email,
+            nickName: req.user.nickName,
+            userName: req.user.userName,
+            phoneNum: req.user.phoneNum,
+          },
+          products,
+        });
+      }
     });
   } catch (error) {
     console.error(error);
@@ -56,49 +37,117 @@ router
   .route('/registration')
   .get(isAuth, (req, res, next) => {
     try {
-      res.render('productRegistration', {
-        user: {
-          email: req.user.email,
-          nickName: req.user.nickName,
-          userName: req.user.userName,
-          phoneNum: req.user.phoneNum,
-          dateOfBirth: req.user.dateOfBirth,
-        },
-      });
+      
+      if (req.user) {
+        res.render('productRegistration', {
+          user: {
+            email: req.user.email,
+            nickName: req.user.nickName,
+            userName: req.user.userName,
+            phoneNum: req.user.phoneNum,
+          },
+    
+        });
+      } else {
+        return res.send(
+          '<script type="text/javascript">alert("로그인된 회원이 아닙니다."); window.location="/product"; </script>'
+        );
+      }
     } catch (error) {
       console.error(error);
       next(error);
     }
   })
-  .post(isAuth, upload.array('productImg', 3), async (req, res, next) => {
-    try {
-      const imgs = req.files.map((v) => {
-        v.path = v.path.slice(6);
-        return v;
-      });
-      const product = await new Product({
-        name: req.body.productName,
-        price: parseInt(req.body.price),
-        description: req.body.description,
-        img: imgs,
-        author: req.user._id,
-      });
-      product.save((err, productInfo) => {
-        if (err) return next(err);
-        return res.send(
-          '<script type="text/javascript">alert("상품이 등록되었습니다."); window.location="/product"; </script>'
-        );
-      });
-    } catch (error) {
-      console.error(error);
-      next(error);
+  .post(
+    isAuth,
+    middleMulter('productImg').array('productImg', 3),
+    async (req, res, next) => {
+      try {
+        const imgs = req.files.map((v) => {
+          v.path = v.path.slice(6);
+          return v;
+        });
+        const product = await new Product({
+          name: req.body.productName,
+          price: parseInt(req.body.price),
+          description: req.body.description,
+          img: imgs,
+          author: req.user.id,
+        });
+        product.save((err, productInfo) => {
+          if (err) return next(err);
+          return res.send(
+            ` <script type="text/javascript">alert("상품이 등록되었습니다."); window.location="/product/${product.id}"; </script>`
+          );
+        });
+      } catch (error) {
+        console.error(error);
+        next(error);
+      }
     }
-  });
+  );
 
-router.get('/infomation', async (req, res, next) => {
-  console.log(req.params._id);
-  const product = await Product.findOne({ where: req.params._id });
-  res.render('productInfo', { product });
+router.get('/:id', isAuth, async (req, res, next) => {
+  try {
+    const product = await Product.findById({ _id: req.params.id });
+    res.render('productDetail', { product, user: req.user });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
+router
+  .route('/:id/update')
+  .get(isAuth, async (req, res, next) => {
+    try {
+      const product = await Product.findById({ _id: req.params.id });
+      res.render('productRegistration', { product, user: req.user });
+    } catch (error) {
+      console.error(error);
+      next(err);
+    }
+  })
+  .post(
+    isAuth,
+    middleMulter('productImg').array('productImg', 3),
+    async (req, res, next) => {
+      try {
+        const imgs = req.files.map((v) => {
+          v.path = v.path.slice(6);
+          return v;
+        });
+        console.log('-------------');
+        console.log(req.params);
+        const product = await Product.findByIdAndUpdate(
+          req.params.id,
+          {
+            name: req.body.productName,
+            price: parseInt(req.body.price),
+            description: req.body.description,
+            img: imgs,
+          },
+          { new: true }
+        );
+        return res.send(
+          ` <script type="text/javascript">alert("상품 정보가 변경되었습니다."); window.location="/product/${product.id}"; </script>`
+        );
+      } catch (error) {
+        console.error(error);
+        next(error);
+      }
+    }
+  );
+
+router.get('/:id/delete', async (req, res, next) => {
+  try {
+    await Product.findByIdAndDelete({ _id: req.params.id });
+    return res.send(
+      ` <script type="text/javascript">alert("상품이 삭제되었습니다."); window.location="/"; </script>`
+    );
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 module.exports = router;
